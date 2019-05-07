a0509e1b81187c4993e2f2fe947b47cfa3dc5167
Mickael Spilmont <mickael.spilmont.fr@gmail.com>
2019-05-07 14:32:03 +0200

    Possibilité de voir le temps d'execution du CRC

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,22 +1,26 @@
 #include "processing.h"
 #include <iostream>
 #include <fstream>
+#include <chrono>
 
 using namespace std;
+using namespace std::chrono;
 
 int main(int argc, char* argv[])
 {
+	// Calcule du temps
+	auto start = high_resolution_clock::now();
 	string filepath{ "C:/Users/mspilmont/Code/CPP/test.txt" };
 
 	if (argc > 1)
 	{
 		filepath = argv[1];
 	}
-	
+
 	ifstream infile(filepath);
 	if (infile)
 	{
-		cout << "L'ouverture du fichier c'est effectuée avec succes" << endl;
+		cout << "L'ouverture du fichier c'est effectuï¿½e avec succes" << endl;
 	}
 	else
 	{
@@ -30,9 +34,12 @@ int main(int argc, char* argv[])
 	{
 		processing.calculator(c);
 	}
-	
+
 	cout << "CRC = " << processing.getRemain() << endl;
 
+	auto stop = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop - start);
+	cout << "Temps d'execution : " << duration.count() << endl;
 	system("PAUSE");
 	return 0;
 }
