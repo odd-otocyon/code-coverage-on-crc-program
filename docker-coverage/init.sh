7cfd4cf2d262243d4090e6d62b966ded57bb4eaa
Mickael Spilmont <mickael.spilmont.fr@gmail.com>
2019-05-13 17:11:25 +0200

    Automatisaton du cross-profiling

--- docker-coverage/init.sh
+++ docker-coverage/init.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-echo "~          Compilation           ~"
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+# Conteneur servant à effectuer des tests de cross-profiling
+
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "~                Compilation                 ~"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 cd /code
 if (ls -d "build")
 then
@@ -10,30 +12,31 @@ then
     echo "Dossier build écrasé"
 fi
 
-mkdir -p build/coverage/cobertura
-mkdir -p build/coverage/html
+mkdir -p build/coverage/
 cd build
 cmake -D CODE_COVERAGE=ON ../
 make
 
-# echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-# echo "~          Déplacement           ~"
-# echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-# mkdir -p /coverage/tests
-# mkdir -p /coverage/src
-# cd /code/build
-# cp -v src/libprocessing.a /coverage/src/libprocessing.a
-# cp -v tests/crc /coverage/tests/crc
-# find ./ -name "*.gcno" -exec mv -v {} /coverage/ \;
+find / -name "*.gcno" -exec mv -v {} /code/build/coverage/ \;
 
-# cd /code
-# rm -rf build
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "~          Déplacement des binaires          ~"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+mkdir -v /test
+mv -v /code/build/tests/crc /test/
 
-# echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-# echo "~             Shell              ~"
-# echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-# cd /coverage
-# echo "Pour lancer le coverage entrez : "
-# echo "gcovr -r ../ --html-details -o coverage/html/index.html"
-sh
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "~                  Coverage                  ~"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+# Variable d'environement du cross-profiling
+echo "Set des varaibles d'environement"
+export GCOV_PREFIX=./
+export GCOV_PREFIX_STRIP=5
 
+cd /test
+./crc crc
+
+find / -name "*.gcda" -exec mv -v {} /code/build/coverage/ \;
+
+cd /code
+gcovr -r ./
